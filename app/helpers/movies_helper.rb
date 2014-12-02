590d107c9553cccb879f957fe55fbb6b4f5b4b7b
percyodi <pearse.2008@gmail.com>
2014-12-02 22:45:56 +0000

    Added vew helpers for better formatting

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+    def format_total_gross(movie)
+        if !movie.flop?
+            "$" + number_to_human(movie.total_gross)
+        else
+            content_tag(:strong, "Flop!")
+        end
+    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+    def flop?
+        self.total_gross < 50000000 or self.total_gross.blank?
+    end
 end
