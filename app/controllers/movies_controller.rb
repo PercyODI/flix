e7936dfc2451b4458ce1e1879a120760093ce22f
percyodi <pearse.2008@gmail.com>
2014-12-05 22:09:53 +0000

    Added custom query on index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
     def index
-        @movies = Movie.all
+        @movies = Movie.released
     end
     
     def show
@@ -13,8 +13,30 @@ class MoviesController < ApplicationController
     
     def update
         @movie = Movie.find(params[:id])
-        movie_params = params.require(:movie).permit!
         @movie.update(movie_params)
         redirect_to movie_path(@movie)
     end
+    
+    def new
+        @movie = Movie.new
+    end
+    
+    def create
+        @movie = Movie.new(movie_params)
+        @movie.save
+        redirect_to @movie
+    end
+    
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_path
+    end
+    
+private
+
+    def movie_params
+        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    end
+    
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
 class Movie < ActiveRecord::Base
     def flop?
-        self.total_gross < 50000000 or self.total_gross.blank?
+        self.total_gross.blank? or self.total_gross < 50000000 
+    end
+    
+    def self.released
+        where("released_on < ?", Time.now).order("released_on desc")
     end
 end
