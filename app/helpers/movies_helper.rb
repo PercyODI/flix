361d74c0060facee97eb668bcf97e5545fe15c17
percyodi <pearse.2008@gmail.com>
2014-12-07 13:46:32 +0000

    added fields to Movie database

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,4 +6,12 @@ module MoviesHelper
             content_tag(:strong, "Flop!")
         end
     end
+    
+    def image_for(movie)
+        if movie.image_file_name.blank?
+            image_tag("http://a1.res.cloudinary.com/wlabs/image/asset/f_auto/placeholder-movie-f515675f5205c172cc9562cd133c6832.png", size:"100")
+        else
+            image_tag(movie.image_file_name)
+        end
+    end
 end
